88733ee5b2185dc2fe874f0a6e72176c96e5c8f9
Reushk <hayek0338@gmail.com>
2026-04-27 16:13:42 +0900

    Update 217_contains_duplicate.py

--- leetcode/Easy/217_contains_duplicate.py
+++ leetcode/Easy/217_contains_duplicate.py
@@ -15,3 +15,18 @@ class Solution:
             return True
         else:
             return False
+""""
+annother solution
+""""
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return True
+            seen.add(i)
+        return False
+
+        
