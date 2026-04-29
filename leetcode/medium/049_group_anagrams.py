c07144f5d9e5f8c6863198e7bd9c0992fb63c577
Reushk <hayek0338@gmail.com>
2026-04-29 21:42:13 +0900

    Create 049_group_anagrams.py

---
+++ leetcode/medium/049_group_anagrams.py
@@ -0,0 +1,22 @@
+""""
+
+difficulty:medium
+
+""""
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for word in strs:
+            count = {}
+            for c in word:
+                count[c] = count.get(c,0) + 1
+
+            key = tuple(sorted(count.items()))
+
+            if key in d:
+                d[key].append(word)
+            else:
+                d[key] = [word]
+        
+        return list(d.values())
